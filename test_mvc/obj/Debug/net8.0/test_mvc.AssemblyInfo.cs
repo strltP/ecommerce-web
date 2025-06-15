610d500f47ec230710f27f0267087c66aacd3c69
strltP <thangnq.bp1@gmail.com>
2025-06-15 15:08:46 +0700

    Fix merge conflict

--- test_mvc/obj/Debug/net8.0/test_mvc.AssemblyInfo.cs
+++ test_mvc/obj/Debug/net8.0/test_mvc.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("test_mvc")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e1cfaaeb8d1e5b9c71a580459c570e3b4cd3ee35")]
 [assembly: System.Reflection.AssemblyProductAttribute("test_mvc")]
 [assembly: System.Reflection.AssemblyTitleAttribute("test_mvc")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
